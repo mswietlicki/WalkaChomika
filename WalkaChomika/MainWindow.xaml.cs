80c5e30e8063adc077951e9d377afcc21156dcaf
Marcin Badurowicz <m@badurowicz.net>
2014-10-29 11:12:05 +0100

    wersja pierwsza z 28 października 2014

---
+++ WalkaChomika/MainWindow.xaml.cs
@@ -0,0 +1,81 @@
+﻿#region License
+/*
+ * Written in 2014 by Marcin Badurowicz <m dot badurowicz at pollub dot pl>
+ *
+ * To the extent possible under law, the author(s) have dedicated
+ * all copyright and related and neighboring rights to this 
+ * software to the public domain worldwide. This software is 
+ * distributed without any warranty. 
+ *
+ * You should have received a copy of the CC0 Public Domain 
+ * Dedication along with this software. If not, see 
+ * <http://creativecommons.org/publicdomain/zero/1.0/>. 
+ */
+#endregion
+using System.Windows;
+
+namespace WalkaChomika
+{
+    /// <summary>
+    /// To jest klasa odpowiadająca głównemu okienku aplikacji
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// To jest funkcja, która uruchamia się w momencie kliknięcia przycisku przez użytkownika
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // tworzymy obiekt klasy Zwierzę, czyli konkretną zmienną
+            // i nadajemy jej atrybuty od razu
+            Zwierzę chomik = new Zwierzę()
+            {
+                Imię = "Pucuś",
+                HP = 10,
+                Mana = 0,
+                Damage = 1
+            };
+
+            // tworzymy nowy obiekt klasy Zwierzę, a potem nadajemy mu po
+            // kolei wartości jego atrybutów dostając się do każdego po kolei
+            Zwierzę pies = new Zwierzę();
+            pies.Imię = "Dino";
+            pies.HP = 100;
+            pies.Mana = 0;
+            pies.Damage = 10;
+            
+            // robimy sobie zmienną do zliczania ugryzień
+            int i = 0;
+
+            // dopóki chomik żyje
+            while (chomik.CzyŻyje())
+            {
+                // pies gryzie chomika - gryzienie zmniejsza ilość HP obiektu chomik
+                pies.Gryź(chomik);
+
+                // liczymy ugryzienia
+                i++;
+            }
+
+            // kiedy chomik umarł, pokazujemy liczbę i w kontrolce o nazwie "kawalekTekstu"
+            kawalekTekstu.Text = i.ToString();
+
+            // i wywołujemy funkcję Test() na klasie MainWindow
+            // powinno się używać this.Test(), ale bez tego też zadziała
+            Test();
+        }
+
+        void Test()
+        {
+            // zmieniamy wartość przezroczystosci kontrolki o nazwie "kawalekTekstu" na 50%
+            kawalekTekstu.Opacity = 0.5;
+        }
+    }
+}
---
+++ WalkaChomika/Zwierzę.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WalkaChomika
+{
+    /// <summary>
+    /// Klasa Zwierzę, reprezentująca zwierzę bojowe
+    /// </summary>
+    class Zwierzę
+    {
+        /// <summary>
+        /// To jest tzw. właściwość. Powinno używać się właściwości zamiast pól, ale dlaczego, to już
+        /// kwestia nieco bardziej zaawansowana, więc chwilowo ją pominiemy. To konkretne pole reprezentuje
+        /// imię zwierzęcia.
+        /// </summary>
+        public string Imię { get; set; }
+
+        /// <summary>
+        /// To pole jest liczbą, która reprezentuje ile zwierzę ma punktów życia
+        /// </summary>
+        public int HP;
+
+        /// <summary>
+        /// To pole reprezentuje ilość many zwierzęcia
+        /// </summary>
+        public int Mana;
+
+        /// <summary>
+        /// To pole reprezentuje maksymalne obrażenia zadawane przez atak
+        /// </summary>
+        public int Damage;
+
+        /// <summary>
+        /// Ta funkcja zwraca, czy zwierze jeszcze żyje, opierając się na danych z tego samego obiektu.
+        /// Funkcje w klasach nazywa się metodami.
+        /// </summary>
+        /// <returns>Zwraca, czy zwierzę jeszcze żyje</returns>
+        public bool CzyŻyje()
+        {
+            if (HP > 0)
+                return true;
+            else
+                return false;
+        }
+
+        /// <summary>
+        /// Ta funkcja atakuje inne zwierzę - podawane jest jako parametr wykonania tej funkcji.
+        /// </summary>
+        /// <param name="z">Zwierzę do zaatakowania</param>
+        public void Gryź(Zwierzę z)
+        {
+            // tworzenie generatora liczb losowych
+            Random r = new Random();
+            // losuje liczbę z zakresu od 0 do maksymalnego ataku obecnego obiektu
+            var moc = r.Next(this.Damage);
+
+            // zwierzęciu przekazanemu jako parametr odejmuje od punktów HP tyle, ile wyniosła moc ataku
+            z.HP = z.HP - moc;
+        }
+    }
+}
