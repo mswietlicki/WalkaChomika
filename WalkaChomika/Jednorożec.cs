a0456b3663378eeed892e07b3906955e9e951ba1
Marcin Badurowicz <m@badurowicz.net>
2014-11-25 19:18:28 +0100

    * zwierzeta latajace i jednorozec

---
+++ WalkaChomika/Jednorożec.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WalkaChomika
+{
+    class Jednorożec : ZwierzęMagiczne, ILatający
+    {
+        public Jednorożec() : base()
+        {
+
+        }
+
+        public Jednorożec(string imię, int mana) : base (imię, mana)
+        {
+            this.HP = 1000;           
+        }
+
+        public bool CzyLata { get; set; }
+
+        public void Lataj()
+        {
+            if (!this.CzyLata)
+            {
+                this.Mana = this.Mana - 10;
+                this.Agility += 1;
+                this.Damage += 10;
+            }
+        }
+    }
+}
